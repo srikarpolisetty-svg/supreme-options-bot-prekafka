348163a8d4d4097f4c47236cc3012c69009e7f35
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2026-01-21 13:49:30 -0500

    fix

--- executionmasterfile.py
+++ executionmasterfile.py
@@ -2,7 +2,7 @@ from execution_functions import get_all_symbols
 import duckdb
 from IBKR_execution import main_execution
 
-DB_PATH = "options_data.db"
+DB_PATH = "/home/ubuntu/supreme-options-bot-prekafka/options_data.db"
 CLIENT_ID = 9001   # pick a fixed, unused client id
 
 con = duckdb.connect(DB_PATH, read_only=True)
